98af44d163be5bc28881abb4f68d59ee3b308fc7
vlad <35404251+tvivlad@users.noreply.github.com>
2020-05-30 10:17:11 +0500

    Новая функция

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Calcul
+    {
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int dif(int x, int y)
+        {
+            return x - y;
+        }
+        public int mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
